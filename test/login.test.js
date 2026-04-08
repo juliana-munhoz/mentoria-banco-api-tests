408f46d04ffbb701ea60b779ff50bf7f8fa43c5c
juliana-munhoz <juliana-munhoz@outlook.com>
2026-04-08 11:55:32 -0300

    Adicionando testes ao login

--- test/login.test.js
+++ test/login.test.js
@@ -17,7 +17,32 @@ describe('Login', ()=>{
 
         })
 
+        it('Deve retornar 400 com mensagem de erro quando usar credenciais invalidas',async ()=>{
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type','application/json')
+                .send({
+                    'username': 'julio.lim',
+                    'senha': '123456'
+                })             
+            expect(resposta.status).to.equal(401);
+            expect(resposta.body.error).to.equal('Usuário ou senha inválidos.')
+        })
+
+        it('Deve retornar 401 com mensagem de erro quando as credenciais forem ausentes',async ()=>{
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type','application/json')
+                .send({
+                    'username': ''          
+                })
+               
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.error).to.equal('Usuário e senha são obrigatórios.')
+        })
     })
+
+    
 
 })
 
